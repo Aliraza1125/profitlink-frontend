2d913112065efabb37ad5eeb9d8386a724668e83
Ali Raza <alirazaasif2511@gmail.com>
2024-11-02 23:27:51 +0600

    first commit

---
+++ src/pages/Auth/admin/AllUsers.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import ServerApi from "../../../api/serverApi";
+
+const AllUsers = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        // Fetch all users from the server with credentials
+        const response = await axios.get(ServerApi.allUsers.url, {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true, // Include credentials in the request
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="container mx-auto px-4 mt-10 md:mt-0">
+      <h2 className="text-xl md:text-2xl font-semibold mb-4 text-center">
+        User Details
+      </h2>
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white rounded-lg shadow overflow-hidden">
+          <thead>
+            <tr>
+              <th className="px-4 py-2 border-b text-xs md:text-sm lg:text-base">
+                Email
+              </th>
+              <th className="px-4 py-2 border-b text-xs md:text-sm lg:text-base">
+                Public Key
+              </th>
+              <th className="px-4 py-2 border-b text-xs md:text-sm lg:text-base">
+                Private Key
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.length > 0 ? (
+              users.map((user) => (
+                <tr
+                  key={user.id}
+                  className="hover:bg-gray-100 text-xs md:text-sm lg:text-base"
+                >
+                  <td className="px-4 py-2 border-b text-center">
+                    {user.email}
+                  </td>
+                  <td className="px-4 py-2 border-b text-center">
+                    {user.publicKey}
+                  </td>
+                  <td className="px-4 py-2 border-b text-center">
+                    {user.privateKey}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan="3" className="text-center py-4">
+                  No user data available
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
---
+++ src/pages/Dashboard/WithdrawalForm.jsx
@@ -0,0 +1,180 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ServerApi from "../../api/serverApi";
+
+import { useSelector } from "react-redux";
+
+const WithdrawalForm = () => {
+  // State variables
+  const [network, setNetwork] = useState("");
+  const [tokenType, setTokenType] = useState("");
+  const [amount, setAmount] = useState("");
+  const [destinationAddress, setDestinationAddress] = useState("");
+  const [successData, setSuccessData] = useState(null);
+  const [errorData, setErrorData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const user = useSelector((state) => state?.user?.user);
+
+  useEffect(() => {
+    handleNetworkChange();
+  }, [tokenType]);
+
+  // Handle network change
+  const handleNetworkChange = () => {
+    if (tokenType === "USDT") {
+      setNetwork("BEP20");
+    }
+
+    if (tokenType === "LOCUS") {
+      setNetwork("ERC20");
+    }
+
+    if (tokenType === "CRETA") {
+      setNetwork("POLYGON");
+    }
+  };
+
+  // Handle form submission (Example logic, replace with actual API call)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!network || !amount || !destinationAddress) {
+      alert("Please fill out all fields.");
+      return;
+    }
+
+    // API call can be made here with the data
+    console.log("Network:", network);
+    console.log("Amount:", amount);
+    console.log("Destination Address:", destinationAddress);
+
+    const formData = {
+      tokenType: tokenType,
+      network,
+      amount,
+      address: destinationAddress,
+      email: user?.email,
+    };
+
+    try {
+      setLoading(true);
+      await axios.post(ServerApi.makeWithdrawal.url, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true,
+      });
+
+      setLoading(false);
+
+      setSuccessData(
+        "Withdrawal request submitted. Soon your request is approved!!"
+      );
+      setNetwork("");
+      setAmount("");
+      setDestinationAddress("");
+
+      setTimeout(() => {
+        setSuccessData("");
+      }, 4000);
+    } catch (error) {
+      if (error.response) {
+        setLoading(false);
+        setErrorData("Withdrawal request Failed");
+        setTimeout(() => {
+          setErrorData("");
+        }, 4000);
+      } else {
+        setLoading(false);
+        setErrorData("Withdrawal request Failed");
+        setTimeout(() => {
+          setErrorData("");
+        }, 4000);
+      }
+      console.error(error);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-[80vh]">
+      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-lg mx-auto">
+        <h2 className="text-2xl font-bold mb-4 text-center">Withdraw Form</h2>
+        {errorData && (
+          <p className="text-red-500 text-center mb-3">{errorData}</p>
+        )}
+        {successData && (
+          <p className="text-green-500 text-center mb-3">{successData}</p>
+        )}
+        {/* Select Token */}
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Select Token
+          </label>
+          <select
+            value={tokenType}
+            onChange={(e) => setTokenType(e.target.value)}
+            className="w-full px-4 py-2 border rounded-md bg-gray-100 text-gray-600 cursor-pointer"
+          >
+            <option value="">-- Select Token --</option>
+            <option value="USDT">USDT</option>
+            <option value="LOCUS">LOCUS </option>
+            <option value="CRETA">CRETA</option>
+          </select>
+        </div>
+
+        {/* Select Network */}
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Select Network
+          </label>
+          <select
+            value={network}
+            onChange={handleNetworkChange}
+            className="w-full px-4 py-2 border rounded-md cursor-pointer"
+          >
+            <option value="">-- Select Network --</option>
+            <option>{network}</option>
+          </select>
+        </div>
+
+        {/* Amount Input */}
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Enter Amount ({tokenType})
+          </label>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="w-full px-4 py-2 border rounded-md"
+            placeholder="Enter amount"
+          />
+        </div>
+
+        {/* Destination Address Input */}
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700 mb-2">
+            Enter Destination Address
+          </label>
+          <input
+            type="text"
+            value={destinationAddress}
+            onChange={(e) => setDestinationAddress(e.target.value)}
+            className="w-full px-4 py-2 border rounded-md"
+            placeholder="Enter destination address"
+          />
+        </div>
+
+        {/* Submit Button */}
+        <button
+          onClick={handleSubmit}
+          className="w-full px-4 py-2 bg-[#16ADAB] text-white font-bold rounded-lg mt-4 hover:bg-[#0e8787]"
+        >
+          {loading ? "Withdrawal requesting..." : "Withdraw"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default WithdrawalForm;
